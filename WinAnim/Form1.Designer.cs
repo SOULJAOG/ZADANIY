f5f3e7ac43b5cb589a0a3ab70df604aad2ee14e4
SOULJAOG <atr.malyykin@gmail.com>
2021-12-02 18:10:15 +0300

    Добавляем класс WinAPIClass

--- WinAnim/Form1.Designer.cs
+++ WinAnim/Form1.Designer.cs
@@ -70,6 +70,7 @@
             this.Controls.Add(this.btnAW_BLEND);
             this.Name = "Form1";
             this.Text = "Анимация формы";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- WinAnim/Form1.cs
+++ WinAnim/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace WinAnim
 {
@@ -15,6 +16,11 @@ namespace WinAnim
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
