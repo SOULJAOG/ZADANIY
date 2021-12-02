0561b57d709bbfc24133eb05eceee3250dc47541
SOULJAOG <atr.malyykin@gmail.com>
2021-12-02 18:22:26 +0300

    Создаем обработчик кнопок

--- WinAnim/Form1.cs
+++ WinAnim/Form1.cs
@@ -22,5 +22,32 @@ namespace WinAnim
         {
 
         }
+
+        private void btnAW_BLEND_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinAPIClass.AnimateWindow(this, 3000, WinAPIClass.WinAnimFlags.AW_ACTIVATE | WinAPIClass.WinAnimFlags.AW_BLEND);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnHOR_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinAPIClass.AnimateWindow(this, 3000, WinAPIClass.WinAnimFlags.AW_HOR_POSITIVE | WinAPIClass.WinAnimFlags.AW_SLIDE);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
+
+        private void btnCenter_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            WinAPIClass.AnimateWindow(this, 3000, WinAPIClass.WinAnimFlags.AW_CENTER | WinAPIClass.WinAnimFlags.AW_SLIDE);
+            this.btnAW_BLEND.Invalidate();
+            this.btnHOR_AW_SLIDE.Invalidate();
+            this.btnCenter_AW_SLIDE.Invalidate();
+        }
     }
 }
