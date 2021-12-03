ba0c2bbdcf6fc85c7d9f0a7078b2b3c656eab806
SOULJAOG <atr.malyykin@gmail.com>
2021-12-03 11:35:19 +0300

    добавяем в приложение методы и делегаты в соответствии с задание

--- WinAsinchDelegate/Form1.cs
+++ WinAsinchDelegate/Form1.cs
@@ -12,9 +12,33 @@ namespace WinAsinchDelegate
 {
     public partial class Form1 : Form
     {
+        private delegate void TineConsumingMethodDelegate(int second);
+        private delegate void SetProgressDelegate(int val);
+        public void SetProgress(int val) { 
+            if (progressBar1.InvokeRequired)
+            {
+                SetProgressDelegate del = new SetProgressDelegate(SetProgress);
+                this.Invoke(del, new object[] { val });
+            }
+            else
+            {
+                progressBar1.Value = val;
+            }
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+        private void TineConsumingMethod(int second)
+        {
+            for (int j = 1; j <= second; j++)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
