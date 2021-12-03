98c4f1ad1b87dc35acede420403977c21a0c0abc
SOULJAOG <atr.malyykin@gmail.com>
2021-12-03 12:31:40 +0300

    создаем метод для сложения двух чисел

--- WinAsynchMethod/Form1.cs
+++ WinAsynchMethod/Form1.cs
@@ -13,6 +13,11 @@ namespace WinAsynchMethod
     public partial class Form1 : Form
     {
         private delegate int AsyncSumm(int x, int y);
+        private int Summ(int a, int b)
+        {
+            System.Threading.Thread.Sleep(9000);
+            return a + b;
+        }
         public Form1()
         {
             InitializeComponent();
