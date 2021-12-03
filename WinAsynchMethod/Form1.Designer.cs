bb2b81a818cd4d78b7bf639bb0f3f0da3dd9530b
SOULJAOG <atr.malyykin@gmail.com>
2021-12-03 12:26:11 +0300

    Дабавляем на форму элементы и устанавливаем им свойства в соответствии с таблицей

--- WinAsynchMethod/Form1.Designer.cs
+++ WinAsynchMethod/Form1.Designer.cs
@@ -28,20 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.rxbB = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnWork = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(8, 24);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(84, 16);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Значение А";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(216, 24);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(84, 16);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Значение В";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // txbA
+            // 
+            this.txbA.Location = new System.Drawing.Point(88, 24);
+            this.txbA.Name = "txbA";
+            this.txbA.Size = new System.Drawing.Size(100, 22);
+            this.txbA.TabIndex = 3;
+            // 
+            // rxbB
+            // 
+            this.rxbB.Location = new System.Drawing.Point(296, 24);
+            this.rxbB.Name = "rxbB";
+            this.rxbB.Size = new System.Drawing.Size(100, 22);
+            this.rxbB.TabIndex = 4;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(16, 64);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Сумма";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // btnWork
+            // 
+            this.btnWork.Location = new System.Drawing.Point(120, 128);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(75, 23);
+            this.btnWork.TabIndex = 6;
+            this.btnWork.Text = "Работа";
+            this.btnWork.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 153);
+            this.ClientSize = new System.Drawing.Size(407, 153);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.rxbB);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
             this.Name = "Form1";
             this.Text = "Асинхронный запуск";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.TextBox rxbB;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnWork;
     }
 }
 
