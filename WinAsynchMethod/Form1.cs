215c9859dbc8dc250cbdab95e3de629da79164d4
SOULJAOG <atr.malyykin@gmail.com>
2021-12-03 12:29:08 +0300

    Создаем делегат

--- WinAsynchMethod/Form1.cs
+++ WinAsynchMethod/Form1.cs
@@ -12,6 +12,7 @@ namespace WinAsynchMethod
 {
     public partial class Form1 : Form
     {
+        private delegate int AsyncSumm(int x, int y);
         public Form1()
         {
             InitializeComponent();
