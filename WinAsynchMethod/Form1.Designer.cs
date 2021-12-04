61204967cd2f6cdb3cbf7aa509f19297171d28ff
SOULJAOG <atr.malyykin@gmail.com>
2021-12-04 08:55:28 +0300

    Добавляем в свойство HelpNameSpace элемента helpProvaider1 название созданного файла, создаес кнопку Help и добавляем для нее обработчик события Click для вызова справки

--- WinAsynchMethod/Form1.Designer.cs
+++ WinAsynchMethod/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.btnRun = new System.Windows.Forms.Button();
             this.btnWork = new System.Windows.Forms.Button();
             this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblA
@@ -109,11 +110,28 @@
             this.btnWork.UseVisualStyleBackColor = true;
             this.btnWork.Click += new System.EventHandler(this.btnWork_Click);
             // 
+            // helpProvider1
+            // 
+            this.helpProvider1.HelpNamespace = "C:\\Users\\art_m\\source\\repos\\SOULJAOG\\ZADANIY\\WinAsynchMethod\\sop.docx";
+            // 
+            // button1
+            // 
+            this.helpProvider1.SetHelpString(this.button1, "StartWork");
+            this.button1.Location = new System.Drawing.Point(321, 128);
+            this.button1.Name = "button1";
+            this.helpProvider1.SetShowHelp(this.button1, true);
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Help";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(407, 153);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnWork);
             this.Controls.Add(this.btnRun);
             this.Controls.Add(this.txbB);
@@ -143,6 +161,7 @@
         private System.Windows.Forms.Button btnRun;
         private System.Windows.Forms.Button btnWork;
         private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
