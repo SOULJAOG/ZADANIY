0cf78010ac4dd7966e8fc2e0a80a8e66a27c8f01
SOULJAOG <atr.malyykin@gmail.com>
2021-12-04 11:35:25 +0300

    Добавляем кнопки и обрабатываем события Click

--- WinBD/Form1.Designer.cs
+++ WinBD/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.oleDbCommand1 = new System.Data.OleDb.OleDbCommand();
             this.oleDbConnection1 = new System.Data.OleDb.OleDbConnection();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -64,11 +65,22 @@
             this.oleDbConnection1.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\art_m\\source\\repos\\SOULJAO" +
     "G\\ZADANIY\\RBProduct.mdb";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(139, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Click here for Exercise 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -82,6 +94,7 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Data.OleDb.OleDbCommand oleDbCommand1;
         private System.Data.OleDb.OleDbConnection oleDbConnection1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
